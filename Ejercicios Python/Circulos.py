cb4ec1d00ec2c6d25b62871337c25aa413e3c6ce
SantiagoFernandezVasquez <160640951+SantiagoFernandezVasquez@users.noreply.github.com>
2024-03-14 18:49:04 -0500

    Add files via upload

---
+++ Ejercicios Python/Circulos.py
@@ -0,0 +1,5 @@
+radio = int(input("Ingrese el radio: "))
+perimetro = (2*3.14) * radio
+area = radio * radio * 3.14
+print (f"El perimetro del circulo es {perimetro}")
+print (f"El area del circulo es {area}")
