3c13c4ba71b5923e46658129610b818d30018ce9
SantiagoFernandezVasquez <160640951+SantiagoFernandezVasquez@users.noreply.github.com>
2024-03-14 18:48:39 -0500

    Delete Circulos.py

--- Circulos.py
+++
@@ -1,5 +0,0 @@
-radio = int(input("Ingrese el radio: "))
-perimetro = (2*3.14) * radio
-area = radio * radio * 3.14
-print (f"El perimetro del circulo es {perimetro}")
-print (f"El area del circulo es {area}")
