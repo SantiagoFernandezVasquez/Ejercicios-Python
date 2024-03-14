e11e87d5f56d9941e57ca01c39c99e7a78bd029e
SantiagoFernandezVasquez <160640951+SantiagoFernandezVasquez@users.noreply.github.com>
2024-03-14 18:51:40 -0500

    Promedio de 4 notas

---
+++ Ejercicios Python/Promedio.py
@@ -0,0 +1,8 @@
+nota1 = int(input("Ingrese el valor de la primera nota: "))
+nota2 = int(input("Ingrese el valor de la segunda nota: "))
+nota3 = int(input("Ingrese el valor de la tercera nota: "))
+nota4 = int(input("Ingrese el valor de la cuarta nota: "))
+
+promedio = (nota1 + nota2 + nota3 + nota4)/4
+
+print(f"El promedio de notas es {promedio}")
